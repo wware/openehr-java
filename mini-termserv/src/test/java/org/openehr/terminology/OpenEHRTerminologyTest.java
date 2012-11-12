afb0cc514b5335d6206044162cc59e7192337af8
Will Ware <wware@alum.mit.edu>
2012-11-12 10:15:40 -0500

    Commit www.openehr.org/svn/ref_impl_java
    http://www.openehr.org/svn/ref_impl_java/
    TRUNK only
    Subversion revision 567
    I wasn't able to get svn2git to work, so no SVN history. Doh.

---
+++ mini-termserv/src/test/java/org/openehr/terminology/OpenEHRTerminologyTest.java
@@ -0,0 +1,66 @@
+package org.openehr.terminology;
+
+import java.util.Set;
+
+import org.openehr.rm.datatypes.text.CodePhrase;
+import org.openehr.rm.support.terminology.*;
+
+import junit.framework.TestCase;
+
+public class OpenEHRTerminologyTest extends TestCase {
+	
+	public void setUp() throws Exception {
+		service = SimpleTerminologyService.getInstance();
+	}
+	
+	public void testHasOpenEHRSettingCode() {
+		
+		TerminologyAccess terminology = service.terminology(
+				TerminologyService.OPENEHR);
+		
+		assertNotNull("openEHR terminology missing", terminology);
+		
+		Set<CodePhrase> codes = terminology.codesForGroupName("setting", "en");
+		
+		assertNotNull("setting(en) missing", codes);
+		
+		CodePhrase home = new CodePhrase("openehr", "225");
+		
+		assertTrue("code 225 (home) doesn't exist..", codes.contains(home));
+	}
+	
+	public void testRubricForCode() throws Exception {
+		TerminologyAccess terminology = service.terminology(
+				TerminologyService.OPENEHR);
+		
+		assertEquals("event", terminology.rubricForCode("433", "en"));		
+		assertEquals("initial", terminology.rubricForCode("524", "en"));		
+	}
+	
+	public void testHasCountryCodes() throws Exception {
+		CodeSetAccess codeSet = service.codeSetForId(
+				OpenEHRCodeSetIdentifiers.COUNTRIES);
+	
+		assertNotNull("countries codeSet missing", codeSet);
+		
+		assertTrue("China missing", 
+				codeSet.hasCode(new CodePhrase("ISO_3166-1", "CN")));
+		assertTrue("Sweden missing", 
+				codeSet.hasCode(new CodePhrase("ISO_3166-1", "SE")));
+		assertTrue("United Kingdom missing", 
+				codeSet.hasCode(new CodePhrase("ISO_3166-1", "GB")));
+		assertTrue("Denmark missing", 
+				codeSet.hasCode(new CodePhrase("ISO_3166-1", "DK")));
+		assertTrue("France missing", 
+				codeSet.hasCode(new CodePhrase("ISO_3166-1", "FR")));
+	}
+	
+	public void testHasCode() throws Exception {
+		TerminologyAccess terminology = service.terminology(
+				TerminologyService.OPENEHR);
+		assertTrue("code for signed missing", terminology.allCodes().contains(
+				new CodePhrase(TerminologyService.OPENEHR, "240")));
+	}
+	
+	TerminologyService service;
+}
